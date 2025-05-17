98753c81c0181f098c35327a5003a74da27fab2b
tkdgh7063 <hoo412298@gmail.com>
2025-05-17 19:19:25 +0900

    Add eventlistener to modify category

--- src/Components/ToDo.tsx
+++ src/Components/ToDo.tsx
@@ -1,12 +1,31 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ id, category, text }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (name: IToDo["category"]) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
