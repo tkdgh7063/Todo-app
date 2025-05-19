24af77fd4a370980a32059a3f82ab17fa4f65812
tkdgh7063 <hoo412298@gmail.com>
2025-05-19 15:43:53 +0900

    Show todos depends on category using selector

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -1,20 +1,28 @@
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelctor, toDoState } from "../atoms";
 import AddTodo from "./AddTodo";
 import ToDo from "./ToDo";
 
 function TodoList() {
-  const toDos = useRecoilValue(toDoState);
+  const toDos = useRecoilValue(toDoSelctor);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(e.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select onInput={onInput} value={category}>
+        <option value="To Do">To Do</option>
+        <option value="Doing">Doing</option>
+        <option value="Done">Done</option>
+      </select>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
       <AddTodo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
     </div>
   );
 }
